d056e470231b1e3c1bcd647f23f3a21ce77088f9
Olga <oolya.kozhevnikova@gmail.com>
2018-08-27 08:31:59 +0530

    Render the map

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      map: '',
+      map: {
+        lat: 12.972442, lng: 77.580643
+      },
       infoWindow: '',
       marker: ''
     }
@@ -16,73 +18,93 @@ class App extends Component {
     const places = [
       {
         'name': 'TRINITY coworking space',
-        'lat': '12.95628',
-        'lng': '77.636616',
         'address': '26/A, 1st floor, Patel Ram Reddy Rd',
-        'phone': '078998 29580'
+        'phone': '078998 29580',
+        'location': {
+          'lat': '12.95628',
+          'lng': '77.636616'
+        }
       },
       {
         'name': 'Bangalore Coworking Hub',
-        'lat': '12.96127',
-        'lng': '77.647449',
         'address': 'A, Behind, 2124, Leela Palace Rd, HAL 3rd Stage, Kodihalli, Bengaluru, Karnataka 560008',
-        'phone': '099024 32565'
+        'phone': '099024 32565',
+        'location': {
+          'lat': '12.96127',
+          'lng': '77.647449'
+        }
       },
       {
         'name': 'Bhive Workspace Indiranagar',
-        'lat': '12.96903',
-        'lng': '77.636409',
         'address': 'No 25, 12th B Main Road, HAL 2nd Stage, Indiranagar, Bengaluru, Karnataka 560038',
-        'phone': '080888 22364'
+        'phone': '080888 22364',
+        'location': {
+          'lat': '12.96903',
+          'lng': '77.636409'
+        }
       },
       {
         'name': 'BHIVE Workspace Koramangala',
-        'lat': '12.940334',
-        'lng': '77.621846',
         'address': 'No. 269, Behind Station, 18th D Main Road, 6th Block, Koramangala, Bengaluru, Karnataka 560095',
-        'phone': '080888 22364'
+        'phone': '080888 22364',
+        'location': {
+          'lat': '12.940334',
+          'lng': '77.621846'
+        }
       },
       {
         'name': 'InstaOffice',
-        'lat': '12.969708',
-        'lng': '77.63611',
         'address': '1st Floor, Citadel Plaza, Indiranagar Double Road, Dopanhalli, Indiranagar, Doopanahalli, Indiranagar, Bengaluru, Karnataka 560008',
-        'phone': '099585 63555'
+        'phone': '099585 63555',
+        'location': {
+          'lat': '12.969708',
+          'lng': '77.63611'
+        }
       },
       {
         'name': 'Beginest',
-        'lat': '12.969328',
-        'lng': '77.640778',
         'address': '783, 1st Cross Rd, 1st Block, HAL 2nd Stage, Doopanahalli, Indiranagar, Bengaluru, Karnataka 560008',
-        'phone': '091080 29762'
+        'phone': '091080 29762',
+        'location': {
+          'lat': '12.969328',
+          'lng': '77.640778'
+        }
       },
       {
         'name': 'Common Desk',
-        'lat': '12.976683',
-        'lng': '77.635627',
         'address': '271, 1st Floor,14th Cross Rd, Eshwara Layout, Indiranagar, Bengaluru, Karnataka 560038',
-        'phone': '099868 88639'
+        'phone': '099868 88639',
+        'location': {
+          'lat': '12.976683',
+          'lng': '77.635627'
+        }
       },
       {
         'name': 'TechHub Bangalore',
-        'lat': '12.933656',
-        'lng': '77.614492',
         'address': '3rd Floor, Salarpuria Business Center, 4th B Cross, 5th Block, Industrial Layout, Bengaluru, Karnataka 560095',
-        'phone': '080507 00113'
+        'phone': '080507 00113',
+        'location': {
+          'lat': '12.933656',
+          'lng': '77.614492'
+        }
       },
       {
         'name': 'Commune Coworks',
-        'lat': '12.933794',
-        'lng': '77.620955',
         'address': '139, First Cross Road, V Block, A Cross Road, Koramangala, Bengaluru, Karnataka 560095',
-        'phone': '088805 00175'
+        'phone': '088805 00175',
+        'location': {
+          'lat': '12.933794',
+          'lng': '77.620955'
+        }
       },
       {
         'name': 'CoWorkIndia',
-        'lat': '12.914581',
-        'lng': '77.638584',
         'address': '#5, 14th Main Road, 15th Cross Rd, HSR Layout, Bengaluru, Karnataka 560102',
-        'phone': '077601 51525'
+        'phone': '077601 51525',
+        'location': {
+          'lat': '12.914581',
+          'lng': '77.638584'
+        }
       }
     ];
 
@@ -92,9 +114,13 @@ class App extends Component {
           <Sidebar 
             places={places}/>
         </div>
-        {/* <div className="main-part" id="map">
-          <MapContainer/>
-        </div> */}
+        <div className="map" id="map">
+          <MapContainer
+            places={places}
+            google={this.state.map}
+            zoom={13}
+            center={this.state.map}/>
+        </div>
       </div>
     );
   }
--- src/Map.js
+++ src/Map.js
@@ -1,26 +1,35 @@
 import React, { Component } from 'react';
 import {Map, InfoWindow, Marker, GoogleApiWrapper} from 'google-maps-react';
+import GoogleMapReact from 'google-map-react';
 
 export class MapContainer extends Component {
   render() {
+    const { places, zoom, center } = this.props
+
+    let title
+    let location
+
+    places.map(place => {
+      title = place.title
+      location = place.location
+    })
+
     return (
-      <Map google={this.props.google} zoom={14}>
-
-        <Marker onClick={this.onMarkerClick}
-          name={'Current location'} />
-
-        <InfoWindow onClose={this.onInfoWindowClose}>
-          <div>
-            {/* <h1>{this.state.selectedPlace.name}</h1> */}
-          </div>
-        </InfoWindow>
-      </Map> 
+      <div style={{ height: '100vh', width: '100%' }}>
+        <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyBSBdeHrWWU7GEbOF54AGQZt81GyLGBzZo' }}
+          defaultCenter={this.props.center}
+          defaultZoom={this.props.zoom}
+        >
+          {/* <AnyReactComponent
+            lat={59.955413}
+            lng={30.337844}
+            text={'Kreyser Avrora'}
+          /> */}
+        </GoogleMapReact>
+      </div> 
     )
   }
 }
 
-export default GoogleApiWrapper({
-  apiKey: ('AIzaSyCEi0rMGRuJNgD1tmvSUkYSfAMM31etpZA')
-})(MapContainer)
-
-// working key: AIzaSyBSBdeHrWWU7GEbOF54AGQZt81GyLGBzZo
+export default MapContainer
